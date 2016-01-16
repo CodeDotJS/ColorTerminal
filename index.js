7b12ab19b44b963fef0be40d7c45daa0992d9641
CodeDotJS <rushforlinux@gmail.com>
2016-01-16 10:55:26 +0530

    testing

---
+++ index.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+var argv = require('minimist')(process.argv.slice(2));
+var ansi = require('ansi-rainbow')
+var chalk = require('chalk')
+
+var ini = argv._.join(' ')
+
+while (ini.length % 160 !== 0) {
+    ini += ' '
+}
+
+var l = ini.length
+
+var spaces = ''
+var i = 0
+for (; i < l; i++) {
+    spaces.length += 2;
+}
+
+ansi
+    .options({
+        color_space: true,
+        gap: l / 160
+    })
+    .skip(ansi._backgrounds)
+    .skip(ansi._colors)
+    .add('bgBlue bgWhite bgRed bgGreen bgBlack bgMagenta bgCyan bgYellow bgBlue bgWhite bgRed bgGreen bgBlack bgMagenta bgCyan bgYellow bgBlue bgWhite bgRed bgGreen bgBlack bgMagenta bgCyan bgYellow bgBlue bgWhite bgRed bgGreen bgBlack bgMagenta bgCyan bgYellow bgBlue bgWhite bgRed bgGreen bgBlack bgMagenta bgCyan bgYellow bgBlue bgWhite bgRed bgGreen bgBlack bgMagenta bgCyan bgYellow bgBlue bgWhite bgRed bgGreen bgBlack bgMagenta bgCyan bgYellow bgBlue bgWhite bgRed bgGreen bgBlack bgMagenta bgCyan bgYellow bgBlue bgWhite bgRed bgGreen bgBlack bgMagenta bgCyan bgYellow bgBlue bgWhite bgRed bgGreen bgBlack bgMagenta bgCyan bgYellow bgBlue bgWhite bgRed bgGreen bgBlack bgMagenta bgCyan bgYellow bgBlue bgWhite bgRed bgGreen bgBlack bgMagenta bgCyan bgYellow bgBlue bgWhite bgRed bgGreen bgBlack bgMagenta bgCyan bgYellow bgBlue bgWhite bgRed bgGreen bgBlack bgMagenta bgCyan bgYellow bgBlue bgWhite bgRed bgGreen bgBlack bgMagenta bgCyan bgYellow bgBlue bgWhite bgRed bgGreen bgBlack bgMagenta bgCyan bgYellow bgBlue bgWhite bgRed bgGreen bgBlack bgMagenta bgCyan bgYellow bgBlue bgWhite bgRed bgGreen bgBlack bgMagenta bgCyan bgYellow bgBlue bgWhite bgRed bgGreen bgBlack bgMagenta bgCyan bgYellow bgBlue bgWhite bgRed bgGreen bgBlack bgMagenta bgCyan bgYellow')
+
+console.log(ansi.bg(spaces))
+console.log(chalk.black(ansi.bg(ini)))
+console.log(ansi.bg(spaces))
