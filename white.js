388dfcff4cbaba497704951b20120e2d2c03e642
CodeDotJS <rushforlinux@gmail.com>
2016-01-19 09:12:15 +0530

    Add white to terminal

---
+++ white.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+var argv = require('minimist')(process.argv.slice(2));
+var ansi = require('ansi-rainbow')
+var chalk = require('chalk')
+
+var ini = argv._.join(' ')
+
+while (ini.length % 160 !== 0) {
+    ini += ' '
+}
+
+var l = ini.length
+
+var spaces = ''
+var i = 0
+for (; i < l; i++) {
+    spaces += ' ';
+}
+
+ansi
+    .options({
+        color_space: true,
+        gap: l / 165
+    })
+    .skip(ansi._backgrounds)
+    .skip(ansi._colors)
+    .add('bgWhite')
+console.log(ansi.bg(spaces))
+console.log(chalk.black(ansi.bg(ini)))
+
+var count = 0;
+for (var tempIn = 0; tempIn < 37; tempIn++) {
+	console.log(ansi.bg(spaces))
+	count+=1
+}
